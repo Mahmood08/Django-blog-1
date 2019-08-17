9ccd1f307b5be9213e7a9bb54e294a430b441b59
Mahmood08 <sirin_ruet@yahoo.com>
2019-08-17 06:56:36 -0700

    updated file

--- mysite/production.py
+++ mysite/production.py
@@ -11,6 +11,6 @@ DATABASES = {
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS'), 'localhost','py230-ubtuntu02004016.westus.cloudapp.azure.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ.get('SECRET_KEY')
